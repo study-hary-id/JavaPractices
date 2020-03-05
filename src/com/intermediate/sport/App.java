590033ed6bb89782c1425aa93d53378cdc0a1987
study-hary-id <haryansyah.study.id@gmail.com>
2020-03-06 05:05:56 +0700

    Update sport App
    Add test view for Athlete
    Add test view for BasketballPlayer
    Add test view for FootballPlayer

--- src/com/intermediate/sport/App.java
+++ src/com/intermediate/sport/App.java
@@ -3,20 +3,78 @@ package com.intermediate.sport;
 public class App {
 
 	public static void main(String[] args) {
-		Player kobeBryant = new Player("Kobe Bryant", "Black Mamba", (short) 1978, "Lakers", (float) 83.7, (byte) 25,
-				1346);
 
-		Player michaelJordan = new Player("Michael Jordan", "MJ", (short) 1963, "Chicago Bulls", (float) 83.5,
-				(byte) 30, 1072);
+//		testInstantiateAthleteAndPrintBiodata();
 
-		Player earvinJohnson = new Player("Earvin Johnson", "Magic", (short) 1959, "Lakers", (float) 84.8, (byte) 19,
-				906);
+//		testPrintBiodataBasketballPlayer();
 
-		Player[] basketballPlayers = { kobeBryant, michaelJordan, earvinJohnson };
-		for (Player player : basketballPlayers) {
-			player.printPlayerInfo();
-			player.freeThrow();
+		printAvailableBasketballPlayer();
 
+//		testPrintBiodataFootballPlayer();
+
+		printAvailableFootballPlayer();
+
+	}
+
+	static void testInstantiateAthleteAndPrintBiodata() {
+		Athlete athlete = new Athlete("Kobe Bryant", "Black Mamba", (short) 1978, "Lakers", 1346);
+		athlete.printBiodata();
+		System.out.println();
+
+		Athlete athleteWithoutNickname = new Athlete("Earvin Johnson", (short) 1959, "Lakers", 906);
+		athleteWithoutNickname.printBiodata();
+	}
+
+	static void testPrintBiodataBasketballPlayer() {
+		BasketballPlayer kobeBryant = new BasketballPlayer("Kobe Bryant", "Black Mamba", (short) 1978, "Lakers", 1346,
+				(float) 83.7, (float) 25.0);
+		kobeBryant.printBiodata();
+		System.out.println();
+
+		BasketballPlayer KBWithoutNickname = new BasketballPlayer("Kobe Bryant", (short) 1978, "Lakers", 1346,
+				(float) 83.7, (float) 25.0);
+		KBWithoutNickname.printBiodata();
+	}
+
+	static void printAvailableBasketballPlayer() {
+		BasketballPlayer kobeBryant = new BasketballPlayer("Kobe Bryant", "Black Mamba", (short) 1978, "Lakers", 1346,
+				(float) 83.7, (float) 25.0);
+
+		BasketballPlayer michaelJordan = new BasketballPlayer("Michael Jordan", "MJ", (short) 1963, "Chicago Bulls",
+				1072, (float) 83.5, (float) 30.1);
+
+		BasketballPlayer earvinJohnson = new BasketballPlayer("Earvin Johnson", "Magic", (short) 1959, "Lakers", 906,
+				(float) 84.8, (float) 19.5);
+
+		BasketballPlayer[] basketballPlayers = { kobeBryant, michaelJordan, earvinJohnson };
+		printAthletesBiodata("Basketball Players", basketballPlayers);
+	}
+
+	static void testPrintBiodataFootballPlayer() {
+		FootballPlayer tomBrady = new FootballPlayer("Tom Brady", "Tom Terrific", (short) 1977, "Tampa Bay", 285, 9988,
+				6377);
+		tomBrady.printBiodata();
+		System.out.println();
+
+		FootballPlayer TBWithoutNickname = new FootballPlayer("Tom Brady", (short) 1977, "Tampa Bay", 285, 9988, 6377);
+		TBWithoutNickname.printBiodata();
+	}
+
+	static void printAvailableFootballPlayer() {
+		FootballPlayer tomBrady = new FootballPlayer("Tom Brady", "Tom Terrific", (short) 1977, "Tampa Bay", 285, 9988,
+				6377);
+
+		FootballPlayer edBrady = new FootballPlayer("Ed Brady", "Ed Terrific", (short) 1979, "New England Patriots",
+				265, 8988, 4377);
+
+		FootballPlayer[] footballPlayers = { tomBrady, edBrady };
+		printAthletesBiodata("Football Players", footballPlayers);
+	}
+
+	static void printAthletesBiodata(String sportsGroup, Athlete[] athletes) {
+		System.out.println(sportsGroup + "\n");
+		for (Athlete athlete : athletes) {
+			athlete.printBiodata();
 			System.out.println();
 		}
 	}
