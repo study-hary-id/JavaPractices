d80cd98d96e1a00a2e83d7ba119bf3be49d26719
study-hary-id <haryansyah.study.id@gmail.com>
2020-03-09 13:54:56 +0700

    Create Daihatsu class

---
+++ src/com/intermediate/vehicle/Daihatsu.java
@@ -0,0 +1,19 @@
+package com.intermediate.vehicle;
+
+public class Daihatsu extends Car {
+
+	public Daihatsu(String model, String type, short yearOfLaunch, String color, String transmission,
+			byte numberOfSeats, byte numberOfDoors, short power, short cubicCapacity, int price, int mileage,
+			boolean brandNew, boolean secondHand, boolean isDamaged) {
+		super("Daihatsu", model, type, yearOfLaunch, color, transmission, numberOfSeats, numberOfDoors, power,
+				cubicCapacity, price, mileage, brandNew, secondHand, isDamaged);
+	}
+
+	// Presentation view
+
+	@Override
+	public void startTheEngine() {
+		System.out.println(super.getFullname() + " engine is started.");
+	}
+
+}
