d034985ce5caac414e28e0542cd07592e359f1a4
study-hary-id <haryansyah.study.id@gmail.com>
2020-03-20 07:49:52 +0700

    Create getRandomWord method

--- src/com/java/game/GameService.java
+++ src/com/java/game/GameService.java
@@ -8,6 +8,14 @@ public final class GameService {
 
 	private static final String SRC_COM_ADVANCED_HANGMAN_WORDS_TXT = "src/com/advanced/hangman/words.txt";
 
+	private static String getRandomWord() throws FileNotFoundException {
+		List<String> words = getTheWords();
+		Random randomNumberGenerator = new Random();
+		int randomIndex = randomNumberGenerator.nextInt(words.size());
+
+		return words.get(randomIndex);
+	}
+
 	private static List<String> getTheWords() throws FileNotFoundException {
 		List<String> words = new ArrayList<>();
 
