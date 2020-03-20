d794196a4d18ff79d1038795e3e59c4fa26537c3
study-hary-id <haryansyah.study.id@gmail.com>
2020-03-20 07:53:58 +0700

    Create start method as main entry

--- src/com/java/game/GameService.java
+++ src/com/java/game/GameService.java
@@ -8,6 +8,56 @@ public final class GameService {
 
 	private static final String SRC_COM_ADVANCED_HANGMAN_WORDS_TXT = "src/com/advanced/hangman/words.txt";
 
+	public static void start() {
+
+		try (Scanner inputScanner = new Scanner(System.in)) {
+
+			String randomWord = getRandomWord();
+			int numberOfChances = randomWord.length();
+			char[] randomWordArray = randomWord.toCharArray();
+			char[] guessingArray = new char[randomWord.length()];
+
+			Arrays.fill(guessingArray, '_');
+
+			System.out.println("Starting the game");
+			System.out.println("The length of the word and the number of chances: " + numberOfChances);
+
+			while (true) {
+				System.out.println("----------------------------------");
+				System.out.print("Please type in a letter: ");
+
+				String inputLine = inputScanner.nextLine().toLowerCase();
+				char letter = inputLine.charAt(0);
+
+				boolean isGuessingCorrect = false;
+				for (int i = 0; i < randomWord.length(); i++) {
+					if (letter == randomWordArray[i]) {
+						guessingArray[i] = letter;
+						isGuessingCorrect = true;
+					}
+				}
+
+				if (isGuessingCorrect) {
+					System.out.println("It was a good guess.");
+				} else {
+					numberOfChances--;
+					if (numberOfChances == 0) {
+						System.out.println("Sorry you ran out of chances. The word was: " + randomWord);
+						System.out.println("Ending the process");
+						break;
+					}
+					System.out.println("Your number of chances is decreased to: " + numberOfChances);
+					System.out.println("Try another letter!");
+				}
+				System.out.println("The word: " + new String(guessingArray));
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	private static String getRandomWord() throws FileNotFoundException {
 		List<String> words = getTheWords();
 		Random randomNumberGenerator = new Random();
